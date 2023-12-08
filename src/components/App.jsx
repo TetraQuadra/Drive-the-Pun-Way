0ddb73e64342d39429d630df7d26c9ea22e73981
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2023-12-08 17:26:56 +0200

    feat: pages routing, 404 redirect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
 import Layout from "layout/Layout";
+import Catalog from "pages/Catalog/Catalog";
+import Favorites from "pages/Favorites/Favorites";
 import Homepage from "pages/Homepage/Homepage";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Homepage />} />
+        <Route path="catalog" element={<Catalog />} />
+        <Route path="favorites" element={<Favorites />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
