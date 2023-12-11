b1dc4d9969eba8bacd683ef7cbea24dfd687fe02
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2023-12-11 10:39:17 +0200

    style: main page minor styles

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -9,7 +9,7 @@ function Homepage() {
                 <div className={styles.headerWrapper}>
                     <h1>Your Journey Starts Here</h1>
                     <p>
-                        ShiftGears is your trusted partner for convenient and reliable car rentals. Our mission is to make
+                        <em>ShiftGears</em> is your trusted partner for convenient and reliable car rentals. Our mission is to make
                         your journey enjoyable and stress-free.
                     </p>
                     <LinkCustom className={styles.startLink} to={'/catalog'}>Start</LinkCustom>
